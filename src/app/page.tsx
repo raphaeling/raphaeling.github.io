18305eb82037071d8c59ed897876af19aca4ef69
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-19 06:02:01 -0700

    added link to 'siya'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-24">
       <div className={`${splineSansMono.className}`}>
         <h1 className="text-4xl">Hi, I'm Raphael Gatchalian.</h1>
-        <h2 className="text-3xl">he/him/siya</h2>
+        <h2 className="text-2xl">he/him/<a className="underline" href="https://en.wiktionary.org/wiki/siya#Tagalog">siya</a></h2>
       </div>
     </main>
   )
