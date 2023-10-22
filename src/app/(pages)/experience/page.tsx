2c647237b8c24493f0ba6875dc1d163de16294db
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-21 17:58:02 -0700

    Fix single quotes

--- src/app/(pages)/experience/page.tsx
+++ src/app/(pages)/experience/page.tsx
@@ -1,9 +1,9 @@
 export default function Experience(): JSX.Element {
   return (
-    <div className="max-w-2xl">
-      <div className="font-display">
-        <h1 className="text-4xl pb-2">Experience.</h1>
-        <h2 className="text-2xl text-light font-thin pb-8">
+    <div className='max-w-2xl'>
+      <div className='font-display'>
+        <h1 className='text-4xl pb-2'>Experience.</h1>
+        <h2 className='text-2xl text-light font-thin pb-8'>
           Things I did.
         </h2>
       </div>
--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,9 +1,9 @@
 export default function Skills(): JSX.Element {
   return (
-    <div className="max-w-2xl">
-      <div className="font-display">
-        <h1 className="text-4xl pb-2">Projects.</h1>
-        <h2 className="text-2xl text-light font-thin pb-8">
+    <div className='max-w-2xl'>
+      <div className='font-display'>
+        <h1 className='text-4xl pb-2'>Projects.</h1>
+        <h2 className='text-2xl text-light font-thin pb-8'>
           Things I made.
         </h2>
       </div>
