b3f0618328b00c153ef70b2f1a2fa9a320bf5405
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-22 07:35:16 -0700

    Added temporary resume drop

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,4 +1,5 @@
 import FadeIn from '@/components/root/fade-in';
+import Link from 'next/link';
 
 export default function Skills(): JSX.Element {
   return (
@@ -9,6 +10,10 @@ export default function Skills(): JSX.Element {
           <h2 className='text-2xl text-light font-thin pb-8'>
             Things I made.
           </h2>
+        </div>
+        <div className='font-thin'>
+          <p>More functionality coming soon!&nbsp;
+            <Link href='assets/resume.pdf' className='transition font-bold text-light hover:text-lighter'>Refer to my resume for now</Link> :)</p>
         </div>
       </div>
     </FadeIn>
--- src/components/experience/experience-body.tsx
+++ src/components/experience/experience-body.tsx
@@ -1,44 +1,10 @@
+import Link from 'next/link';
+
 export default function ExperienceBody(): JSX.Element {
   return (
-    <div>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
-      <p>slay.</p>
+    <div className='font-thin'>
+      <p>More functionality coming soon!&nbsp;
+        <Link href='assets/resume.pdf' className='transition font-bold text-light hover:text-lighter'>Refer to my resume for now</Link> :)</p>
     </div>
   );
 }
