0cdf0a951797d7aa01dea297f4569ad713daed9f
Raphael Gatchalian <rlgatchalian@gmail.com>
2023-10-27 03:12:09 -0700

    Added footer navigation

--- src/components/root/Footer.tsx
+++ src/components/root/Footer.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link';
 import ExternalLink from './ExternalLink';
 
 export default function Footer() {
   return (
-    <div className='px-12 lg:px-24 font-body text-md text-dark font-thin justify-center pt-6 sm:pt-12 pb-6'>
+    <div className='px-12 lg:px-24 font-body text-md text-dark font-thin justify-center pt-6 pb-6'>
       <p>
         Made with ♡ + matcha using&nbsp;
         <ExternalLink href='https://nextjs.org/' underline className='hover:text-light'>
@@ -20,6 +21,27 @@ export default function Footer() {
         <ExternalLink href='https://github.com/raphaeling/raphaeling.github.io' underline className='hover:text-light'>
           source code
         </ExternalLink>
+      </p>
+      <p className='mt-2 font-display text-sm'>
+        <Link href='/' className='transition hover:text-light'>
+          about
+        </Link>
+        &nbsp;•&nbsp;
+        <Link href='/experience' className='transition hover:text-light'>
+          experience
+        </Link>
+        &nbsp;•&nbsp;
+        <Link href='/projects' className='transition hover:text-light'>
+          projects
+        </Link>
+        &nbsp;•&nbsp;
+        <Link href='/skills' className='transition hover:text-light'>
+          skills
+        </Link>
+        &nbsp;•&nbsp;
+        <Link href='/more' className='transition hover:text-light'>
+          more
+        </Link>
       </p>
     </div>
   );
